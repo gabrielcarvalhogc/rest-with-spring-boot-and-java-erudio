981d207e7182b0fe7aac94c2e8e59b421d4d0afe
gabrielGC <gabriel225gc@gmail.com>
2024-10-01 15:11:23 -0300

    criada a classe person com os métodos get

---
+++ src/main/java/br/com/erudio/services/PersonService.java
@@ -0,0 +1,48 @@
+package br.com.erudio.services;
+
+import br.com.erudio.model.Person;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+@Service
+public class PersonService {
+    private final AtomicLong counter = new AtomicLong();
+    private Logger logger = Logger.getLogger(PersonService.class.getName());
+
+    public List<Person> findAll() {
+        List<Person> persons = new ArrayList<>();
+        for (int i=0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+
+    public Person findById(String id) {
+        logger.info("Finding one person!");
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Leandro");
+        person.setLastName("costa");
+        person.setAddress("Minas Gerais");
+        person.setGender("Male");
+        return person;
+    }
+
+
+    private Person mockPerson(int i) {
+        logger.info("Finding all people!");
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Person name: " + i);
+        person.setLastName("Last name: " + i);
+        person.setAddress("Some address in Brazil: " + i);
+        person.setGender("Male");
+        return person;
+    }
+}
